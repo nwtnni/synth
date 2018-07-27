4a42c82ed78ba33f950b4a1283410f923d2590cb
Newton Ni <nwtnni@gmail.com>
2018-07-26 23:27:23 -0400

    Implement naive convolution

--- src/data.rs
+++ src/data.rs
@@ -1,13 +1,13 @@
 use num_traits::NumOps;
 
-pub struct Waveform<I: NumOps, T: Iterator<Item = I>>(T);
+pub struct Waveform<I: NumOps + Copy, T: Iterator<Item = I>>(T);
 
-impl <I: NumOps, T: Iterator<Item = I>> Waveform<I, T> {
+impl <I: NumOps + Copy, T: Iterator<Item = I>> Waveform<I, T> {
     pub fn from(iter: T) -> Self {
         Waveform(iter)
     }
 
-    pub fn transform<F, J: NumOps, O: Iterator<Item = J>>(self, f: F) -> Waveform<J, O> where F: Fn(T) -> O {
+    pub fn transform<F, J: NumOps + Copy, O: Iterator<Item = J>>(self, f: F) -> Waveform<J, O> where F: Fn(T) -> O {
         Waveform(f(self.0))
     }
 
@@ -22,9 +22,23 @@ impl <I: NumOps, T: Iterator<Item = I>> Waveform<I, T> {
     pub fn take(self, count: usize) -> Waveform<I, impl Iterator<Item = I>> {
         Waveform(self.0.take(count))
     }
+
+    pub fn convolve<Y: Iterator<Item = I>>(self, waveform: Waveform<I, Y>) -> Waveform<I, impl Iterator<Item = I>> {
+        let lhs = self.into_iter().collect::<Vec<_>>();
+        let rhs = waveform.into_iter().collect::<Vec<_>>();
+        let mut output = Vec::with_capacity(lhs.len() + rhs.len());
+
+        for (i, l) in lhs.iter().enumerate() {
+            for (j, r) in rhs.iter().enumerate() {
+                output[i + j] = *l * *r;
+            }
+        }
+
+        Waveform(output.into_iter())
+    }
 }
 
-impl <I: NumOps, T: Iterator<Item = I>> IntoIterator for Waveform<I, T> {
+impl <I: NumOps + Copy, T: Iterator<Item = I>> IntoIterator for Waveform<I, T> {
     type Item = I;
     type IntoIter = T;
     fn into_iter(self) -> Self::IntoIter { self.0 }
