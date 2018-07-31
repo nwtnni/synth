ccce69ce656c35af739d44809ed02a0fd2940d54
Newton Ni <nwtnni@gmail.com>
2018-07-31 09:48:53 -0400

    Make envelope iterator return functions

--- src/envelope.rs
+++ src/envelope.rs
@@ -1,4 +1,13 @@
-pub enum Envelope {
+use config::SAMPLE_RATE;
+
+pub struct Envelope {
+    shape: Shape,
+    time: f64,
+    start: f64,
+    stop: f64,
+}
+
+pub enum Shape {
     Line {
         offset: f64, 
         slope: f64,
@@ -10,11 +19,48 @@ pub enum Envelope {
 }
 
 impl Envelope {
-    pub fn linear(offset: f64, slope: f64) -> Self {
-        Envelope::Line { offset, slope }
+    pub fn linear(offset: f64, slope: f64, start: f64, stop: f64) -> Self {
+        Envelope {
+            shape: Shape::Line {
+                offset,
+                slope
+            },
+            time: 0.0,
+            start,
+            stop,
+        }
     }
 
-    pub fn exponential(ratio: f64) -> Self {
-        Envelope::Exp { ratio }
+    pub fn exponential(ratio: f64, start: f64, stop: f64) -> Self {
+        Envelope {
+            shape: Shape::Exp { ratio },
+            time: 0.0,
+            start,
+            stop,
+        }
+    }
+}
+
+impl Iterator for Envelope {
+
+    type Item = Box<Fn(f64, f64) -> f64>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let time = self.time;
+
+        self.time += 1.0 / SAMPLE_RATE;
+
+        if time < self.start || time > self.stop { return None }
+
+        Some(
+            match self.shape {
+                | Shape::Line{offset, slope} => {
+                    Box::new(move |initial, _| offset + initial + (slope * time))
+                }
+                | Shape::Exp{ratio} => {
+                    Box::new(move |_, current| current * ratio)
+                }
+            }
+        )
     }
 }
